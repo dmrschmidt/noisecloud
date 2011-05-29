fb33a5c4344a3611d7b16577d45af4234bba26be
Dennis Schmidt <metzeltiger@web.de>
2011-05-29 06:59:57 +0200

    made a mixer patch using a gain mixer

---
+++ client/js/patches/mix_patch.js
@@ -0,0 +1,15 @@
+var MixPatch = function(params) {
+	MixPatch.superclass.constructor.call(this, params);
+};
+_extend(MixPatch, Patch, {
+	
+	name: "mix_patch",
+	
+	// fake an actual mixer
+	init: function() {
+		this.node = yana.audioContext.createGainNode();
+		this.node.gain.value = 1;
+	},
+	
+});
+Yana.registerPatch(MixPatch);
